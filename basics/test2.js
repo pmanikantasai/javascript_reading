452530dea59eb1ff495ef89b0bbe44efe8221a6c
manikantasai <pallapumanikantasai@gmail.com>
2026-03-25 09:11:13 +0000

    done

--- basics/test2.js
+++ basics/test2.js
@@ -1,9 +1,20 @@
 const accountId = 112233
+
+/*here constant use as const
+Once declear the const,we never can change assign value */
+
 let accountName ="MAnikantasai"
+
+/* let is used to mention varible that can follow ("use strict") 
+here scoope is important*/
+
 var accountPassword ="manikanta"
 accountMail="pallapumanikantasai@gmail.com"
 let accountState;
+/*if in javascript without mention any value 
+for variable its shows undefined */
 
 console.log(accountId);
 console.table([accountId,accountName,accountPassword,accountMail,accountState])
-/*if in javascript without mention any value for variable its shows undefined */
+/* console.table can make output in taabular form */
+
