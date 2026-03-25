2932e54345612e4d635335da76eda5e063728a6c
manikantasai <pallapumanikantasai@gmail.com>
2026-03-25 08:54:30 +0000

    done

---
+++ basics/test2.js
@@ -0,0 +1,9 @@
+const accountId = 112233
+let accountName ="MAnikantasai"
+var accountPassword ="manikanta"
+accountMail="pallapumanikantasai@gmail.com"
+let accountState;
+
+console.log(accountId);
+console.table([accountId,accountName,accountPassword,accountMail,accountState])
+/*if in javascript without mention any value for variable its shows undefined */
