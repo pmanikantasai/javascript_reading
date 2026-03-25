53d4e47f642336d30fc493906bcd237e9d5b85bc
manikantasai <pallapumanikantasai@gmail.com>
2026-03-25 09:27:25 +0000

    done

---
+++ basics/test3.js
@@ -0,0 +1,33 @@
+// DATA TYPES
+
+//Primitive Data Types
+/*Primitives are the simplest forms of data. They are immutable, 
+meaning the value itself cannot be changed */
+
+let names="manikanta" // string
+let num=22 // int
+let f =3.14 // float
+let bigint=387466278998376 // 2 to 2power53
+let b = true // boolean
+let c = false // boolean
+let x; //undefined
+let y=null //null
+// symbol
+
+
+// Non-Primitive (Reference) Types
+
+// object is collections of key-value pairs.
+
+const user = {
+  name: "Alex",
+  age: 25,
+  isMember: true
+};
+
+// typeof 
+console.log(typeof"manikanta");
+
+console.table([names,num,f,bigint,b,c,x,y])
+
+
