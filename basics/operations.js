e170de643c77d45d64eada1687fc6072b074f10a
manikantasai <pallapumanikantasai@gmail.com>
2026-03-26 05:33:27 +0000

    operations is done

---
+++ basics/operations.js
@@ -0,0 +1,27 @@
+let x=25
+let negx= -x
+
+console.log(negx);// # -25
+
+// console.log(2+1); # 3
+// console.log(2-1); # 1
+// console.log(4/2); # 2
+// console.log(3/2); # 1.5
+//console.log(3%2); # 1 its reminder
+
+let str1 ="hello"
+let str2 =" mani"
+let str3 = str1+str2
+
+console.log(str3);
+
+console.log("1"+2); // 2 is converted into string
+console.log("2"+4+4);// 4,4 also converted into string
+console.log(10+"5"+2);// here 10,2 into strings
+console.log(1+2+"3");// 1+2 =3 then 3 is converted into string we get 33
+
+
+
+
+
+
