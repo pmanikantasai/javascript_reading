0d3e2d1c3d2747fff56225350f42e87465b67644
manikantasai <pallapumanikantasai@gmail.com>
2026-03-26 10:24:30 +0000

    strings is done

---
+++ basics/strings1.js
@@ -0,0 +1,13 @@
+let myName ="Manikantasai"
+
+// console.log(myName)
+// console.log(myName.__proto__);
+// console.log(myName[0]);
+console.log(myName.toUpperCase());
+console.log(myName.length);
+
+let urName = "mani"
+let age =23
+console.log(`hi ${urName},your age is${age}`)
+
+
