83838ba472e55125fa8b23b1a43acb7c595bc956
manikantasai <pallapumanikantasai@gmail.com>
2026-03-26 06:07:06 +0000

    done

---
+++ basics/comparsion.js
@@ -0,0 +1,19 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+
+console.log("2">1);
+console.log(2>"1");
+
+console.log(null >0);
+console.log(null == 0);
+console.log(null>=0)
+
+console.log("2"==2); // here the data types are converted
+
+console.log("2"===2)// but here strict check on data type we get false
+
+
+
