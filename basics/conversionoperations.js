cfbedfee0e0ffad1662e7a9285468108de547732
manikantasai <pallapumanikantasai@gmail.com>
2026-03-26 05:05:13 +0000

    done

---
+++ basics/conversionoperations.js
@@ -0,0 +1,39 @@
+let marks=33
+
+console.log(typeof(marks));
+console.log(typeof marks);
+
+let x="59"
+console.log(typeof x);
+// if u want to convert the string into num then
+let valueInNumber= Number(x)
+console.log(typeof valueInNumber);
+
+let y="23ab"
+
+// we are trying to convert
+let con=Number(y)
+console.log(typeof con);  //here output is number but print the con and y
+console.log(y)// here we get value that 23ab
+console.log(con)// but here we get NaN
+//It is a special value in JavaScript that represents an invalid number result.
+
+let a=null
+console.log(typeof a);// output is 0
+
+//boolean 1=> true ,0=> false or "" => false
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
