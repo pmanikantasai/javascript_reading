f760e756c5f0881ba866ea349ae1dbde3f71333d
manikantasai <pallapumanikantasai@gmail.com>
2026-03-30 10:24:02 +0000

    dates done

--- basics/dates.js
+++ basics/dates.js
@@ -1,1 +1,12 @@
-console.log("main")
+let myDate =  new Date();
+console.log(myDate);
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toTimeString());
+console.log(myDate.toLocaleString());
+console.log(myDate.toLocaleDateString());
+console.log(myDate.toLocaleTimeString());
+console.log(myDate.getFullYear());
+console.log(myDate.getMonth());
+console.log(myDate.getDate());
+console.log(myDate.getDay());
