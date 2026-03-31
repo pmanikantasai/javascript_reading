1bf2c4b85f80062d5baa738aa9db4cb38012c294
manikantasai <pallapumanikantasai@gmail.com>
2026-03-31 06:13:38 +0000

    done

--- basics/dates.js
+++ basics/dates.js
@@ -1,12 +1,25 @@
-let myDate =  new Date();
-console.log(myDate);
-console.log(myDate.toString());
-console.log(myDate.toDateString());
-console.log(myDate.toTimeString());
-console.log(myDate.toLocaleString());
-console.log(myDate.toLocaleDateString());
-console.log(myDate.toLocaleTimeString());
-console.log(myDate.getFullYear());
-console.log(myDate.getMonth());
-console.log(myDate.getDate());
-console.log(myDate.getDay());
+ let myDate =  new Date();
+
+//  console.log(myDate);
+//  console.log(myDate.toString());
+//  console.log(myDate.toDateString());
+//  console.log(myDate.toTimeString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.getFullYear());
+// console.log(myDate.getMonth());
+// console.log(myDate.getDate());
+// console.log(myDate.getDay());
+
+// let myCrtDate = new Date(2002, 0, 28); // in js month starts with 0, so 0 is January, 1 is February and 2 is March
+// console.log(myCrtDate);
+// console.log(myCrtDate.toString());
+// let myCreateDate = new Date(2002, 3, 28);
+// console.log(myCreateDate);
+// console.log(myCreateDate.toString());
+
+// console.log(typeof myDate);
+// console.log(myDate.getMonth());
+// console.log(myDate.getDay());
+console.log(myDate.toLocaleString('default', { month: "2-digit"}));
