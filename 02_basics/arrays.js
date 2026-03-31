9258db6b9152f577699554e3bc53cc1b4b1bdfc4
manikantasai <pallapumanikantasai@gmail.com>
2026-03-31 06:59:35 +0000

    done

---
+++ 02_basics/arrays.js
@@ -0,0 +1,38 @@
+const myArray = [1, 2, 3, 4, 5]; // here we create an array with 5 elements
+
+// console.log(myArray); // this will print the entire array
+
+// // arrays starts with 0 index
+// console.log(myArray[0]); // this will print the first element of the array (1)
+// console.log(myArray[1]); // this will print the second element of the array (2)
+// console.log(myArray[2]); // this will print the third element of the array (3)
+// console.log(myArray[3]); // this will print the fourth element of the array (4)
+// console.log(myArray[4]); // this will print the fifth element of the array (5)
+
+// const strArray = ["Hello", "World", "JavaScript", "Arrays"];// array can be strings
+
+// const newArray= new Array(7,8,9,10)
+// console.log(newArray);
+// console.log(newArray[0]);
+
+/***************Method***************/
+// push method - adds an element to the end of the array
+// myArray.push(6);
+// myArray.push(7)
+// myArray.push(8)
+// pop method - removes the last element of the array
+//myArray.pop(); 
+// unshift method - adds an element to the beginning of the array
+//myArray.unshift(0);
+// shift method - removes the first element of the array
+//myArray.shift();
+// include method - checks if an element is present in the array
+// console.log(myArray.includes(5));// we get true because 5 is present in the array
+// console.log(myArray.includes(10));// we get false because 10 is not present in the array
+// console.log(myArray.indexOf(3));// indexOf method - returns the index of the first occurrence of an element in the array
+// console.log(myArray.indexOf(10)); // we get -1 because 10 is not present in the array
+const n1Array = [3,5,1,9,0,12,11,14,7,8,6,4];
+// console.log(n1Array.slice(2,5));
+// console.log(n1Array.splice(0,4));// splice method - removes elements from an array and returns the removed elements
+// console.log(n1Array); // after splice, n1Array will have the remaining elements
+console.log(n1Array.sort());// sort method - sorts the elements of an array in place and returns the sorted array
