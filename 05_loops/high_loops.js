9d2f6b766069e2b180a11009f119a329d4b22f97
manikantasai <pallapumanikantasai@gmail.com>
2026-04-06 07:16:49 +0000

    done

--- 05_loops/high_loops.js
+++ 05_loops/high_loops.js
@@ -29,6 +29,43 @@ const myobject = {
 
 for (const key in myobject) {
     //console.log(key);
-    console.log(`${key}=>${myobject[key]}`);
+    //console.log(`${key}=>${myobject[key]}`);
     
 }
+
+// foreach is a method that executes a provided function once for each array element
+const codingLanguages = ["JavaScript", "Python", "Java", "C++"];
+codingLanguages.forEach(function(language){
+   // console.log(language);
+})
+
+codingLanguages.forEach((language) => {
+   // console.log(language);
+});
+
+codingLanguages.forEach((item,index,arr)=>{
+    //console.log(`Item: ${item}, Index: ${index}, Array: ${arr}`);
+    //console.log(item,index,arr);
+    
+})
+
+const coding=[
+    {"codename": "python",
+    "filename": "python.py"},
+    {"codename": "javascript",
+    "filename": "javascript.js"},
+    {"codename": "java",
+    "filename": "java.java"},
+    {"codename": "c++",
+    "filename": "c++.cpp"}
+]
+
+coding.forEach((item)=>{
+    //console.log(item.filename);
+   
+})
+//console.log("=====================================")
+coding.forEach((item)=>{
+    //console.log(item.codename);
+   
+})
