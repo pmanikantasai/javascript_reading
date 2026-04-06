8ce36be020812c021168e1d030686cc648679817
manikantasai <pallapumanikantasai@gmail.com>
2026-04-06 06:18:29 +0000

    done

--- 05_loops/high_loops.js
+++ 05_loops/high_loops.js
@@ -1,12 +1,34 @@
 // for of means "for each element of the array, do this"
 let myArray = [1, 2, 3, 4, 5];
 for (let element of myArray) {
-    console.log(element);
+   // console.log(element);
 }
 
 // for in means "for each property of the object, do this"
 let myObject = {a: 1, b: 2, c: 3};
 for (let property in myObject) {
-    console.log(property); // this will print the property name
-    console.log(property + ": " + myObject[property]);
+    //console.log(property); // this will print the property name
+    //console.log(property + ": " + myObject[property]);
+}
+
+// MAP is a method that creates a new array by applying a function to each element of the original array
+const map =new Map();
+map.set("IN", "India");
+map.set("US", "United States");
+map.set("UK", "United Kingdom");
+
+for (const [key,values] of map) {
+   // console.log(key + " : " + values);
+}
+
+const myobject = {
+    "TL":"TELUGU",
+    "EN":"ENGLISH",
+    "HI":"HINDI"
+}
+
+for (const key in myobject) {
+    //console.log(key);
+    console.log(`${key}=>${myobject[key]}`);
+    
 }
